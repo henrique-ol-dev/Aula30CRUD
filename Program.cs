5af618bc3318c55d6173b6b9b785cac3c2e9eafc
henrique-ol-dev <riqueneguin467@gmail.com>
2020-07-04 12:44:43 -0300

    Aula30CRUD

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula27_28_29_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto p1 = new Produto();
+            p1.Codigo = 1;
+            p1.Nome = "Tagima";
+            p1.Preco = 7500f;
+
+            p1.Cadastrar(p1);
+            p1.Remover("Tagima");
+
+            Produto alterado = new Produto();
+            alterado.Codigo = 2;
+            alterado.Nome = "ESP";
+            alterado.Preco = 126000f;
+
+            p1.Alterar(alterado);
+
+            List<Produto> lista = p1.Ler();
+
+            foreach(Produto item in lista)
+            {
+                Console.WriteLine($"R$ {item.Preco} - {item.Nome}");
+            }
+
+        }
+    }
+}
